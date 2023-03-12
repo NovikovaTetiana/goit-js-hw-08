b61f9e43f74af4b35c9456dd81002843272bd2c5
Tetiana Novikova <113465579+NovikovaTetiana@users.noreply.github.com>
2023-03-12 18:48:29 +0200

    first hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryElem = document.querySelector(".gallery");
+
+function createMarkupItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__item" 
+      href="${original}">
+      <img 
+      class="gallery__image" 
+      src="${preview}"
+      alt="${description}" />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+const markup = createMarkupItems(galleryItems);
+galleryElem.insertAdjacentHTML("beforeend", markup);
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
